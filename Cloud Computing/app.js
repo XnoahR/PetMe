82fd52c9b6b5e2db34b1d8bb71f94d9d140889c5
Rio Saputro <rio.ae23@gmail.com>
2023-12-01 02:53:41 +0700

    Show data with public access bucket file

--- Cloud Computing/app.js
+++ Cloud Computing/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {Storage} = require('@google-cloud/storage');
-const app = express();
 const path = require("path");
+const ejs = require("ejs");
+const app = express();
 const port = process.env.PORT || 3000;
 const { getConnection, getBook, pool } = require("./db");
 // Use let instead of const for variables that might be reassigned
 let conn;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "views")));
 const credentials = new Storage({
   keyFilename: path.join(__dirname, './credentials/pomomato-deploy-a8a3a6822d62.json'),
   projectId: 'pomomato-deploy',
@@ -42,16 +45,38 @@ app.get("/", async (req, res) => {
   // getFiles();
 });
 
+app.get("/test", async (req, res) => {
+  try {
+    const [files] = await bucket.getFiles({
+      prefix: `${folderName}/`, // Set the folder path as the prefix
+    });
 
+    if (files.length > 0) {
+      console.log(`Files in ${folderName} folder:`);
+      files.forEach(file => {
+        console.log(file.name);
+      });
+    } else {
+      console.log(`No files found in ${folderName} folder.`);
+    }
+  } catch (error) {
+    console.error('Error getting file list:', error);
+  }
+}
+);
 
 app.get("/get", async (req, res) => {
+  express.query = req.query;
+  
     try {
       const [file] = await bucket.file(path.join(folderName, fileName).replace(/\\/g, '/')).get();
   
       if (file) {
         const filePath = file.name;
         //respond change link to file
-        res.status(200).send(`https://storage.googleapis.com/tomadoro/${filePath}`);
+        res.status(200)
+        .render("./index.ejs", { dataLink: `https://storage.googleapis.com/tomadoro/${filePath}` });
+        
       } else {
         console.log(`File not found: ${path.join(folderName, fileName).replace(/\\/g, '/')}`);
       }
